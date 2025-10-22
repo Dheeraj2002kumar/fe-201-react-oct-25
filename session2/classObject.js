cc51b2568204618718f99aea07c31d4d1acfb85d
Dheeraj Kumar <dk8549644@gmail.com>
2025-10-23 00:08:04 +0530

    update session2 topics

---
+++ session2/classObject.js
@@ -0,0 +1,9 @@
+// Method & Contructor overriding
+// super keyword
+// classes can inherit from other Classes
+// class enable inheritance
+// How contructor function enable inheritance !
+// Prototyple inheritance in js
+// Utilities available on the Object function
+// Object Contructor function
+// function are just objects
