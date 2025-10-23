121b1b3d52cd229c339a49cd31c65b475fd13868
Dheeraj Kumar <dk8549644@gmail.com>
2025-10-23 17:05:55 +0530

    update session 3 agenda

---
+++ session3/functionalProgramming.js
@@ -0,0 +1,14 @@
+// - Modules
+//     - Todo application
+//         - Pure function
+//     - side effect
+//     - immutability
+//     - objects
+//     - arrays
+//     - HOF-- > Higher Order function
+// - JS -- > Functional Programming | Function -> First class objects
+// - Imperative vs Declarative code
+
+// -------------------------------
+// - FP(Functional Programming) --> composing functions
+// JS supports both Functional programming & OOP!
---
+++ session3/readme.md
@@ -0,0 +1,15 @@
+# JavaScript [session 3]
+
+## Agenda
+
+- Modules
+- Todo application
+- Pure function
+    - side effect
+    - immutability
+    - objects
+    - arrays
+- HOF --> Higher Order function
+- JS --> Functional Programming | Function -> First class objects
+- Imperative vs Declarative code
+- FP (Functional Programming)
